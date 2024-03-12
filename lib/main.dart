210a7f0fd7f337b21a038dddcc445cc5a9fe2d82
Yakshith Salian <144924386+Yakshith966@users.noreply.github.com>
2024-03-12 09:07:35 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,54 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:vendorgrocery/Screen/RegisterScreen.dart';
+import 'package:vendorgrocery/Screen/onboardscreen.dart';
+import 'package:vendorgrocery/Screen/welcomescreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.deepOrangeAccent
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+//this is the main screen
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+   
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => OnBoardScreen()));
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Center(
+        child: Hero(tag: 'Logo', child: Image.asset('assets/images/logo2.png')),
+      ),
+    );
+  }
+}
 
